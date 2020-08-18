05fc8f20ee19ae337b2c16fb952347059d2c109d
Paul Kruglov <cokeyjoe@DESKTOP-8JSEIAN.localdomain>
2020-08-18 13:21:35 +0300

    feature(cmd/main): added main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,46 @@
 package main
 
+import (
+	"binarytree/pkg/api"
+	"binarytree/pkg/logging"
+	"binarytree/pkg/logging/bstlogger"
+	"binarytree/pkg/logging/httplog"
+	"binarytree/pkg/tree/utils"
+	"flag"
+	"log"
+	"os"
+)
+
 func main() {
+
+	path := flag.String("path", "ints.json", "path to ints json file")
+
+	f, err := os.Open(*path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bst, err := utils.FromReader(f)
+
+	// NOTE: немного не смотрится здесь, но дефереом закрывать - будет висеть пока не остановится сервер
+	// при этом файл прочитан, все остальное - не его забота
+	f.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	jsonLogger := logging.New(os.Stdout)
+
+	loggedBst := bstlogger.NewBSTLogger(bst, jsonLogger)
+
+	httpLogger := httplog.New(jsonLogger)
+
+	httpAPI := api.New(loggedBst, ":8000")
+	httpAPI.Use(httpLogger.LogHTTP)
+
+	if err := httpAPI.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
 
 }
